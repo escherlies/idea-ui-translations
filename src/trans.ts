b8d585935bf2a7ea50843d44620e9cd50bf986f9
Enrico Scherlies <e.scherlies@pm.me>
2023-01-17 17:13:19 +0100

    Add shadowDOM to trans web component

--- src/trans.ts
+++ src/trans.ts
@@ -4,11 +4,29 @@ const trans = new Map()
 trans.set("HTuFzfsQpom58JYmLZeWo", "Hello, World!")
 trans.set("Mlvc0CGpoEUsVSNml20dA", "Very, nice!")
 
-
+/**
+ * Translation WebComponent
+ * 
+ * Parcel v1 
+ * 
+ * Use shadow doom, see https://stackblitz.com/edit/customelements?file=app.js
+ *                  and https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM
+ * 
+ */
 class Trans extends HTMLElement {
+  shadowDOM: ShadowRoot
+
   constructor() {
     super()
-    this.innerText = trans.get(this.getAttribute("trans-key"))
+    this.shadowDOM = this.attachShadow({ mode: "open" })
+  }
+
+  connectedCallback() {
+    this.setContent(trans.get(this.getAttribute("trans-key")))
+  }
+
+  setContent(content: string) {
+    this.shadowDOM.textContent = content
   }
 }
 
